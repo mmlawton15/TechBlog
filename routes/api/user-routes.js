407e0d2242bdad8d879fe323ff429ac2c32eedd9
Mary Margaret <mmlawton15@gmail.com>
2022-02-10 10:18:22 -0500

    adding password login authentication

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -45,8 +45,28 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email!' });
+      return;
+    }
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password '});
+      return;
+    }
+    res.json({ user: dbUserData, message: 'Login Successful' });
+  })
+})
+
 router.put('/:id', (req, res) => {
     User.update(req.body, {
+      individualHooks: true,
       where: {
         id: req.params.id
       }
