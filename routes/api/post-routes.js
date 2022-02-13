aedd6836edef05ea5f668be238f4cb8432c5ae8f
Mary Margaret <mmlawton15@gmail.com>
2022-02-13 17:18:29 -0500

    find one post, figured out that my posts for some reason are auto incrememnting from 2. whatevs

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Post, User } = require('../../models');
 
+//FIND ALL POSTS
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
@@ -11,14 +12,14 @@ router.get('/', (req, res) => {
         'created_at'
       ],
       include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
+        // {
+        //   model: Comment,
+        //   attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        //   include: {
+        //     model: User,
+        //     attributes: ['username']
+        //   }
+        // },
         {
           model: User,
           attributes: ['username']
@@ -30,6 +31,36 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
+});
+
+//FIND A SINGLE POST
+router.get('/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'post_url', 'title', 'created_at'],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(dbPostData => {
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+    res.json(dbPostData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
   });
+});
+
+//CREATE A POST
+
 
 module.exports = router;
