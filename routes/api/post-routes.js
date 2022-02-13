9879864ad0e73162f704a6b79b63c6e5840e7e0b
Mary Margaret <mmlawton15@gmail.com>
2022-02-13 18:39:36 -0500

    added the option to edit a post

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -61,6 +61,42 @@ router.get('/:id', (req, res) => {
 });
 
 //CREATE A POST
+router.post('/', (req, res) => {
+  Post.create({
+    title: req.body.title,
+    post_url: req.body.post_url,
+    user_id: req.body.user_id
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
+//EDIT A POST TITLE
+router.put('/:id', (req, res) => {
+  Post.update(
+    {
+      title: req.body.title
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(dbPostData => {
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+    res.json(dbPostData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+})
 
 module.exports = router;
