37164f8fd6a8ad6423b628e24737d711dcb964fe
Mary Margaret <mmlawton15@gmail.com>
2022-02-14 08:54:19 -0500

    got template engine to render a post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,17 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('homepage');
-});
+    res.render('homepage', {
+      id: 1,
+      post_url: 'https://handlebarsjs.com/guide/',
+      title: 'Handlebars Docs',
+      created_at: new Date(),
+      vote_count: 10,
+      comments: [{}, {}],
+      user: {
+        username: 'testing_user'
+      }
+    });
+  });
 
 module.exports = router;
