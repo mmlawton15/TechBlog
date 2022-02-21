0dbda5ced9f9749a8ee51f440d254d2e92cc74fa
Mary Margaret <mmlawton15@gmail.com>
2022-02-21 09:48:51 -0500

    updating pseudocode

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 //login.js
 // THEN my user credentials are saved and I am logged into the site
 
-//homepage.handlebars
-// DONE - WHEN I click on the homepage option in the navigation
-// DONE - THEN I am taken to the homepage and presented with existing blog posts that include the post title and the date created
-
 //Post.js
 // WHEN I click on an existing blog post
 // THEN I am presented with the post title, contents, post creator’s username, and date created for that post and have the option to leave a comment
-
-//Comment.js
-// WHEN I enter a comment and click on the submit button while signed in
-// THEN the comment is saved and the post is updated to display the comment, the comment creator’s username, and the date created
 
 //dashboard docs - gets data and posts an html page
 // WHEN I click on the dashboard option in the navigation
