a207a925890007bf6456aeb3d9678da12f3d990f
Mary Margaret <mmlawton15@gmail.com>
2022-02-21 09:27:21 -0500

    pulling from main

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,6 +35,8 @@ router.get('/', (req, res) => {
     });
 });
 
+
+//LOGIN ROUTE
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/');
@@ -42,5 +44,48 @@ router.get('/login', (req, res) => {
     }
     res.render('login');
 });
+
+
+//GET A SINGLE PAGE
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at'
+      ],
+        include: [
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+    })
+        .then(dbPostData => {
+          if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+          }
+          const post = dbPostData.get({ plain: true });
+          res.render('single-post', { post });
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+});
+
 
 module.exports = router;
