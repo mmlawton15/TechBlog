bf799e67cfabf23daeac361426390dc8ac9f69cc
Mary Margaret <mmlawton15@gmail.com>
2022-02-21 09:49:10 -0500

    got the comments to work as anticipated! yay

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -14,16 +14,18 @@ router.get('/', (req, res) => {
 
 //CREATE A COMMENT
 router.post('/', (req, res) => {
+  if (req.session) {
     Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id
     })
     .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
         console.log(err);
         res.status(400).json(err);
     });
+  }
 });
 
 //DELETE A COMMENT
